e23c5a605e6e48e726d7567bb344ea3e33d3cd7e
Susantha Kumara <dksusantha@outlook.com>
2024-12-23 15:59:14 +0530

    IeImplemented the POST request for Product

--- src/Services/Catelog/Catelog.API/Models/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catelog/Catelog.API/Models/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,29 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
 
 namespace Catelog.API.Models.Products.CreateProduct;
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-    :IRequest<CreateProductResult>;
+    :ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        // TODO: Implement
-        throw new NotImplementedException();
+        // create product entity from command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+
+
+        // save to database
+
+
+        // return CreateProductResult result
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
